4d003ff151bed9921a8221ce461afe3484732594
ayse <120752957+ayse-solmaz@users.noreply.github.com>
2026-08-30 01:17:22 +0300

    add go day 1 module with hello world and core types

---
+++ learn/go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello,academy")
+
+	var year int = 2026
+
+	var score float64 = 9.5
+
+	name := "aysnu"
+
+	ready := true
+
+	fmt.Println(year, score, name, ready)
+
+}
