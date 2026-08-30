538e6dcb35d45e891b2db01cde6f26410d36c59b
ayse <120752957+ayse-solmaz@users.noreply.github.com>
2026-08-30 11:24:14 +0300

    Add Go day 10 Shape and Logger interface practice.

--- learn/go/main.go
+++ learn/go/main.go
@@ -1,37 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type User struct {
-	Name string
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+type Circle struct {
+	R float64
 }
 
-func (u User) Role() string {
-	return "member"
+func (c Circle) Area() float64 {
+	return math.Pi * c.R * c.R
+}
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.R
 }
 
-func (u User) Greet() string {
-	return "hi " + u.Name
+type Rectangle struct {
+	W, H float64
 }
 
-type Admin struct {
-	User
-	Level int
+func (r Rectangle) Area() float64 {
+	return r.W * r.H
+}
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.W + r.H)
+}
+func printShape(name string, s Shape) {
+	fmt.Println(name, "area", s.Area(), "peri", s.Perimeter())
 }
 
-func (a Admin) Role() string {
-	return "admin"
+type Logger interface {
+	Log(msg string)
 }
+type ConsoleLogger struct{}
+
+func (ConsoleLogger) Log(msg string) {
+	fmt.Println("log:", msg)
+}
+
+type NoopLogger struct{}
+
+func (NoopLogger) Log(string) {}
 
 func main() {
-	a := Admin{
-		User:  User{Name: "ada"},
-		Level: 3,
-	}
+	c := Circle{R: 1}
+	rect := Rectangle{W: 2, H: 3}
+	printShape("circle", c)
+	printShape("rect", rect)
 
-	fmt.Println("promoted Name:", a.Name)
-	fmt.Println("promoted Greet:", a.Greet())
-	fmt.Println("outer Role:", a.Role())
-	fmt.Println("inner Role:", a.User.Role())
-	fmt.Println("level:", a.Level)
+	wantArea := 6.0
+	gotArea := rect.Area()
+	fmt.Println("rect area table", "want", wantArea, "got", gotArea, "ok", gotArea == wantArea)
+
+	var logs Logger = ConsoleLogger{}
+	logs.Log("shapes done")
+	logs = NoopLogger{}
+	logs.Log("you should not see this")
 }
